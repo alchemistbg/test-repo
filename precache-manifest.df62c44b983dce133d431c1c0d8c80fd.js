a53ba1455f51c299fdaf54d7599983066d1401c4
oggy-bogy <oggy@uni-plovdiv.net>
2020-03-10 17:22:58 +0200

    Updates

---
+++ precache-manifest.df62c44b983dce133d431c1c0d8c80fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3370d4bc673aac12428b95186344a2e3",
+    "url": "/test-repo/index.html"
+  },
+  {
+    "revision": "539fad12995643e2ba9a",
+    "url": "/test-repo/static/css/2.0b45a2dc.chunk.css"
+  },
+  {
+    "revision": "f286967a53269cd9dc7e",
+    "url": "/test-repo/static/css/main.b99c448d.chunk.css"
+  },
+  {
+    "revision": "539fad12995643e2ba9a",
+    "url": "/test-repo/static/js/2.5f93c706.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/test-repo/static/js/2.5f93c706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f286967a53269cd9dc7e",
+    "url": "/test-repo/static/js/main.f80f0e50.chunk.js"
+  },
+  {
+    "revision": "6876294a493f794801f2",
+    "url": "/test-repo/static/js/runtime-main.e7031bb9.js"
+  }
+]);
