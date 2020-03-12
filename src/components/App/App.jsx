0cc2757829fcbcda3860a90f0ff98e790c3658b6
oggy-bogy <oggy@uni-plovdiv.net>
2020-03-12 23:42:49 +0200

    Update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,7 +27,7 @@ function App() {
 				<CartProvider value={useCartState}>
 					<SortProvider value={useSortState}>
 						<Header />
-						{/* <h1>HELLO</h1> */}
+						<h1>HELLO</h1>
 						<main className="site-main">
 							<Switch>
 								<Route exact path='./' render={() => <ProductList products={db.products} />} />
