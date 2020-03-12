fac1eb87f82cbb1f45a394f59b0ac5711c64f237
oggy-bogy <oggy@uni-plovdiv.net>
2020-03-12 23:37:53 +0200

    Update docs folder

---
+++ docs/build/precache-manifest.8e88c393c713f1b220bc1f5eb754a231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fc9f00a80a9c4bca724162c2c04055",
+    "url": "/test-repo/index.html"
+  },
+  {
+    "revision": "539fad12995643e2ba9a",
+    "url": "/test-repo/static/css/2.0b45a2dc.chunk.css"
+  },
+  {
+    "revision": "a8fcd70c5b7de2d255ee",
+    "url": "/test-repo/static/css/main.b99c448d.chunk.css"
+  },
+  {
+    "revision": "539fad12995643e2ba9a",
+    "url": "/test-repo/static/js/2.5f93c706.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/test-repo/static/js/2.5f93c706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fcd70c5b7de2d255ee",
+    "url": "/test-repo/static/js/main.42de3244.chunk.js"
+  },
+  {
+    "revision": "6876294a493f794801f2",
+    "url": "/test-repo/static/js/runtime-main.e7031bb9.js"
+  }
+]);
